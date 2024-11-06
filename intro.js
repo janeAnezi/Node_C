a4913cef1d5dc77e0dfe8ef4992da94e82978d51
Anezijane <ahnnieanezi@gmail.com>
2024-11-06 10:08:01 +0100

    create dateTime module

--- intro.js
+++ intro.js
@@ -1,6 +1,10 @@
 var http = require('http')
 
+var date = require('./dateTimeModule')
+
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('Hello Helen, Welcome to Node')
+    res.write(date.dateTime())
+    res.end()
+    // res.end('Hello Helen, Welcome to Node')
 }).listen(8000);
