e18d32720ace0b440079601c610f21f067187a99
Anezijane <ahnnieanezi@gmail.com>
2024-11-06 23:25:14 +0100

    add response data on the server

--- intro.js
+++ intro.js
@@ -4,7 +4,6 @@ var date = require('./dateTimeModule')
 
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(date.dateTime())
-    res.end()
-    // res.end('Hello Helen, Welcome to Node')
+    res.write(date.dateTime() + ' Hello Helen, Welcome to Node')
+     res.end()
 }).listen(8000);
